823c3a852af1c2801d7af53a46c6ea26518d772e
Saleh Al-Saleh <saleh.alsaleh802@gmail.com>
2023-10-07 12:43:06 +0300

    finished hide and show form feature

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function addBookToLibrary(name, outher, pageNum){
   let book = new Book(name, outher, pageNum);
   myLibrary.push(book);
 }
+
+let bookForm= document.querySelector('.BookForm');
+let addBookBtn = document.querySelector('.addButton');
+let cancelBtn = document.querySelector('#cancel');
+
+cancelBtn.addEventListener('click', ()=>{
+  bookForm.classList.remove('bookformStyle');
+  addBookBtn.classList.remove('hide');
+
+});
+
+addBookBtn.addEventListener('click',()=>{
+  if(bookForm.classList.contains('bookformStyle')){
+  bookForm.classList.remove('bookformStyle');
+  addBookBtn.classList.remove('hide');
+  }
+else{
+  bookForm.classList += ' bookformStyle';
+  addBookBtn.classList += ' hide';
+}
+})
+
